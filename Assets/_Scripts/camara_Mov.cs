88b27722d0bf8147b406c085438de30ff466873d
Madpong <55111300+Madpong@users.noreply.github.com>
2020-06-13 22:49:32 +0100

    Character Move and Look at Mouse

---
+++ Assets/_Scripts/camara_Mov.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara_Mov : MonoBehaviour
+{
+    public Transform playerPos;
+
+    private void FixedUpdate() {
+
+        transform.position = new Vector3(playerPos.position.x, transform.position.y, playerPos.position.z-4f);
+
+    }
+}

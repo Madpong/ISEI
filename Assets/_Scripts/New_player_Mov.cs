8b58195048f34c022f68ef0c3222d9d7daca4f7b
rot <rubiobr@tutamail.com>
2020-06-18 22:00:04 +0100

    Animated Body - Not working

---
+++ Assets/_Scripts/New_player_Mov.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_player_Mov : MonoBehaviour
+{
+    //Character Movement and Speed 
+    public Animator animator;
+    public CharacterController controller;
+    public float speed = 6.0f;
+    private Vector3 direction;
+
+    /*public float dashCD = 5000;
+    public float dashTime = 0; 
+    private bool isDash = false;
+    public Vector3 dashMove;
+    */
+    private void Start() {
+
+    }
+
+
+    private void Update()
+    {
+        //Use arrow or AWSD as input and save in horizontal and vertical
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Create a vector3 for save Horizontal an Vertical, Normalized to smooth
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+       if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetBool("dodge", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            animator.SetBool("dodge", false);
+        }
+       /*
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isDash = false;
+        
+        } else {
+            dashTime -= Time.deltaTime;
+            ///Debug.Log("dashTime" + dashTime);
+        }
+
+       
+         if (dashTime <= 0)
+        {
+
+            //isDash = false;
+        }
+        else {
+
+           // dashTime -= Time.deltaTime;
+
+        }
+        */
+        
+            //
+            //Debug.Log("START DASH");
+
+
+        /** else {
+            dashTime -= Time.deltaTime;
+        }**/
+
+
+    }
+    private void FixedUpdate()
+    {
+
+        //This condicion check if the direction variable have new movements from our keys
+        //If true we update the position of our player
+       // if (isDash) {
+          // controller.Move(direction * speed * 8 * Time.deltaTime);
+           
+        //}
+        
+        if (direction.magnitude >= 0.1f) {
+                
+            controller.Move(direction * speed * Time.deltaTime);
+            animator.SetTrigger("isWalk");
+           
+        } 
+        
+
+    }
+/*
+    void dash() {
+
+         { 
+            if (dashTime <= 0){
+
+            Debug.Log("Dash");
+                dashTime = dashCD;
+                //speed = 20f;
+                dashTime = dashCD;
+            }
+        }
+
+
+    }
+*/
+
+} 
+
+
+/**if (isDash)
+            {
+                dash();
+isDash = false;
+         
+**/
